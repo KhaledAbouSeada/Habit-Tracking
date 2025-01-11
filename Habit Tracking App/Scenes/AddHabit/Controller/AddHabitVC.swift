d30d40ce7691ed1b353024ebcb98b139ed9d607a
github.com (khaled abouseada) <khaled.a.seada@gmail.com>
2025-01-11 14:20:08 +0200

    Base project with firebase pod and readme file

---
+++ Habit Tracking App/Scenes/AddHabit/Controller/AddHabitVC.swift
@@ -0,0 +1,29 @@
+//
+//  AddHabitVC.swift
+//  Habit Tracking App
+//
+//  Created by Khaled on 11/01/2025.
+//
+
+import UIKit
+
+class AddHabitVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
